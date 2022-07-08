1ee3f35fe925bac4c215816bc2fe43ec9dda28b9
kyleriley.jpeg <kylecriley92@gmail.com>
2022-07-08 13:42:32 -0600

    commit on 'master'

--- dal.js
+++ dal.js
@@ -1,82 +1,65 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
-let db            = null;
- 
+const url = 'mongodb://localhost:27017';
+let db = null;
 
-MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
+// connect to mongo
+MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
     console.log("Connected successfully to db server");
+
+    // connect to myproject database
     db = client.db('myproject');
 });
 
-
-function create(name, email, password){
-    return new Promise((resolve, reject) => {    
-        const collection = db.collection('users');
-        const doc = {name, email, password, balance: 0};
-        collection.insertOne(doc, {w:1}, function(err, result) {
-            err ? reject(err) : resolve(doc);
-        });    
-    })
+// create user account using the collection.insertOne function
+function create(name, email, password) {
+    // TODO: populate this function based off the video
 }
 
-
-function getBalance(userID) {
+// find user account 
+function find(email) {
     return new Promise((resolve, reject) => {
-        const customer = db
-            .collection('users')
-            .find({"userID": userID})
-            .toArray(function(err,user) {
-                err ? reject(err) : resolve (user);
-            });
-    })
-}
-
-
-
-function updateUserBalance(userID, newBalance){
-    return new Promise((resolve, reject) => {    
-        const customer = db
-            .collection('users')            
-            .updateOne({"userID": userID }, {$set : {"balance" : newBalance} })
-            .then((result) => {
-                console.log(result)
-                resolve(result)
-            })
-            .catch((err) => {
-                console.log(err)
-                reject(err)
-            });  
-    });   
-}
-
-
-
-function updateActivity(userID, depositDateTime,newBalance ) {
-    return new Promise((resolve, reject) => {
-        const customer = db
-            .collection('users')
-            .updateOne({"userID" : userID}, {$push : {"activity" : {"datetime" : depositDateTime, "balance" : newBalance}}})
-            .then((result) => {
-                console.log(result)
-                resolve(result)
-            })
-            .catch((err) => {
-                console.log(err)
-                reject(err)
-            });
-    })
-}
-
-function all(){
-    return new Promise((resolve, reject) => {    
         const customers = db
             .collection('users')
-            .find({})
-            .toArray(function(err, docs) {
+            .find({ email: email })
+            .toArray(function (err, docs) {
                 err ? reject(err) : resolve(docs);
-        });    
+            });
     })
 }
 
+// find user account
+function findOne(email) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .findOne({ email: email })
+            .then((doc) => resolve(doc))
+            .catch((err) => reject(err));
+    })
+}
 
-module.exports = {create, getBalance, updateUserBalance, updateActivity, all};
+// update - deposit/withdraw amount
+function update(email, amount) {
+    return new Promise((resolve, reject) => {
+        const customers = db
+            .collection('users')
+            .findOneAndUpdate(
+                { email: email },
+                { $inc: { balance: amount } },
+                { returnOriginal: false },
+                function (err, documents) {
+                    err ? reject(err) : resolve(documents);
+                }
+            );
+
+
+    });
+}
+
+// return all users by using the collection.find method
+function all() {
+    // TODO: populate this function based off the video
+}
+
+
+module.exports = { create, findOne, find, update, all };
--- mongo_test.js
+++ mongo_test.js
@@ -1,15 +1,19 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
-
+ 
+// connect to mongo
 MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
-    console.log('Connected!')
+  console.log("Connected successfully to server");
 
+    // database Name
     const dbName = 'myproject';
     const db = client.db(dbName);
 
+    // new user
     var name = 'user' + Math.floor(Math.random()*10000);
     var email = name + '@mit.edu';
 
+    // insert into customer table
     var collection = db.collection('customers');
     var doc = {name, email};
     collection.insertOne(doc, {w:1}, function(err, result) {
@@ -22,7 +26,8 @@ MongoClient.connect(url, {useUnifiedTopology: true}, function(err, client) {
         .toArray(function(err, docs) {
             console.log('Collection:',docs);
 
-            client.close();
+            // clean up
+            client.close();            
+    });    
 
-        });
 });
